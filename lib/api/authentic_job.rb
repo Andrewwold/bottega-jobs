4c6772d98b723cf2262caa9475b258ca313af0ac
Joon <joon@slatedev.com>
2017-03-24 12:55:41 -0600

    added pry for pretty formatting yo

--- lib/api/authentic_job.rb
+++ lib/api/authentic_job.rb
@@ -2,13 +2,13 @@ require 'net/https'
 module Api
 	module AuthenticJob
 
-		def get
+		def self.get
 			uri = URI('https://authenticjobs.com/api/')
-			params = { :limit => 10, :page => 3 }
+			params = { :limit => 10, :page => 3, format: 'json', api_key: '83485a746390c45a90c9646cfe72f881', method: 'aj.jobs.search' }
 			uri.query = URI.encode_www_form(params)
 
 			res = Net::HTTP.get_response(uri)
-			puts res.body if res.is_a?(Net::HTTPSuccess)
+			res.body if res.is_a?(Net::HTTPSuccess)
 		end
 	end
 end
